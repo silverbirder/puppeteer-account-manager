955c769fd59f90781f176fa546257e50d5197624
shibamoto <silverbirder@gmail.com>
2020-05-09 23:15:27 +0900

    😎 Add download image function

--- stash/gravatar.ts
+++ stash/gravatar.ts
@@ -1,14 +1,26 @@
 import * as crypto from "crypto";
 import fetch from 'node-fetch';
+import * as fs from "fs";
+import * as util from "util";
+import {pipeline} from 'stream';
+
+
+const streamPipeline = util.promisify(pipeline);
 
 const md5hex = str => crypto.createHash('md5').update(str).digest('hex');
 
 const profileRequest = async email => {
-    const hash = md5hex(email);
-    const gUrl = `https://www.gravatar.com/${hash}`;
-    const pUrl = await fetch(gUrl).then((r) => r.url);
-    const profile = await fetch(`${pUrl}.json`);
-    return profile.json();
+    const pUrl = await fetch(`https://www.gravatar.com/${md5hex(email)}`).then((r) => r.url);
+    return (await fetch(`${pUrl}.json`)).json();
 };
 
-export {profileRequest}
+const download = async (url, path) => {
+    await fetch(url).then(res => {
+        if (!res.ok) {
+            throw new Error(`unexpected response ${res.statusText}`);
+        }
+        return streamPipeline(res.body, fs.createWriteStream(path));
+    });
+};
+
+export {profileRequest, download}
--- stash/script2.ts
+++ stash/script2.ts
@@ -1,6 +1,8 @@
-import {profileRequest} from "./gravatar";
+import {download, profileRequest} from "./gravatar";
 
 (async () => {
     const res = await profileRequest(process.env.GRAVATAR_EMAIL);
-    console.log(res.entry[0]);
+    const imageUrl = `${res.entry[0].thumbnailUrl}.png`;
+    const name = res.entry[0].preferredUsername;
+    await download(imageUrl, `./${name}.png`);
 })();
