a82be21fe53eddba19774a666e11108f0a99dd0b
shibamoto <silverbirder@gmail.com>
2020-05-10 00:01:30 +0900

    😎 Add download image function

--- stash/gravatar.ts
+++ stash/gravatar.ts
@@ -4,14 +4,42 @@ import * as fs from "fs";
 import * as util from "util";
 import {pipeline} from 'stream';
 
+interface IProfile {
+    name: {
+        firstName: string,
+        familyName: string,
+    },
+    intro: string,
+    location: string,
+    user: string,
+    thumbnailUrl: string,
+    urls: [{value: string, title: string}],
+    photos: {
+        value: string,
+        type: string,
+    },
+}
 
 const streamPipeline = util.promisify(pipeline);
 
 const md5hex = str => crypto.createHash('md5').update(str).digest('hex');
 
-const profileRequest = async email => {
-    const pUrl = await fetch(`https://www.gravatar.com/${md5hex(email)}`).then((r) => r.url);
-    return (await fetch(`${pUrl}.json`)).json();
+const profileRequest = async (email: string): Promise<IProfile> => {
+    const pUrl: string = await fetch(`https://www.gravatar.com/${md5hex(email)}`).then((r) => r.url);
+    const profile: any = await (await fetch(`${pUrl}.json`)).json();
+    const entry: any = profile.entry[0];
+    return {
+        name: {
+            firstName: entry.name.givenName,
+            familyName: entry.name.familyName,
+        },
+        intro: entry.aboutMe,
+        location: entry.currentLocation,
+        photos: entry.photos,
+        thumbnailUrl: entry.thumbnailUrl,
+        urls: entry.urls,
+        user: entry.preferredUsername,
+    }
 };
 
 const download = async (url, path) => {
--- stash/script2.ts
+++ stash/script2.ts
@@ -2,7 +2,5 @@ import {download, profileRequest} from "./gravatar";
 
 (async () => {
     const res = await profileRequest(process.env.GRAVATAR_EMAIL);
-    const imageUrl = `${res.entry[0].thumbnailUrl}.png`;
-    const name = res.entry[0].preferredUsername;
-    await download(imageUrl, `./${name}.png`);
+    await download(res.thumbnailUrl, `./${res.user}.png`);
 })();
