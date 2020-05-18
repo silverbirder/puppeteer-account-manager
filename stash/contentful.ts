a134e84af084783fd27ac222077633ce0067cbef
shibamoto <silverbirder@gmail.com>
2020-05-19 07:38:45 +0900

    😎 Add Contentful service

--- stash/contentful.ts
+++ stash/contentful.ts
@@ -1,8 +1,31 @@
 import * as contentful from 'contentful'
-import {Entry, Field} from "contentful";
-import * as util from "util";
+import {Entry, EntryCollection} from 'contentful';
+import * as fs from 'fs';
+import * as util from 'util';
 import {pipeline} from 'stream';
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
+
+interface IContentProfile {
+    type: string,
+    firstName: string,
+    lastName: string,
+    avatar: string,
+    aboutMe: string,
+    address: string,
+    url: Array<{ title: string, value: string }>
+}
+
+const newIContentProfile = (): IContentProfile => {
+    return {
+        aboutMe: '',
+        address: '',
+        avatar: '',
+        firstName: '',
+        lastName: '',
+        type: '',
+        url: []
+    };
+};
 
 const client = contentful.createClient({
     space: process.env.CONTENTFUL_SPACE,
@@ -10,29 +33,38 @@ const client = contentful.createClient({
 });
 const streamPipeline = util.promisify(pipeline);
 
-const profileRequest = async (id: string): Promise<any> => {
-    const entry: Entry<any> = await client.getEntry(id);
-    const fields: Field = entry.fields;
-    return Object.entries(fields).map((field, _) => {
-        const keyName = field[0];
-        const value = field[1];
-        let returnValue = {name: keyName, value: undefined};
-        if (value.hasOwnProperty('sys') && value.sys.type === 'Asset') {
-            // For Asset
-            returnValue.value = `https:${value.fields.file.url}`;
-        } else if (value.hasOwnProperty('nodeType') && value.nodeType === 'document') {
-            // For Rich Document
-            returnValue.value = value.content[0].content.map((content) => {
-                if (content.hasOwnProperty('value')) {
-                    return content.value;
-                }
-                return content.content[0].value;
-            }).join('');
-        } else {
-            returnValue.value = value;
-        }
-        return returnValue;
+const profileRequest = async (): Promise<IContentProfile> => {
+    const entryCollection: EntryCollection<IContentProfile> = await client.getEntries({
+        content_type: 'profile',
+        'fields.type': 'BASE',
     });
+    if (entryCollection.total !== 1) {
+        throw Error('Please prepare the only 1 data. ' +
+            'content_type: profile\n' +
+            'fields.type: BASE')
+    }
+    const entry: Entry<any> = entryCollection.items[0];
+    let resultValue: IContentProfile = newIContentProfile();
+    Object.entries(entry.fields).map((field, _) => {
+        const key: string = field[0];
+        const value: any = field[1];
+        switch (key) {
+            case 'avatar':
+                resultValue.avatar = `https:${value.fields.file.url}`;
+                break;
+            case 'aboutMe':
+                resultValue.aboutMe = value.content[0].content.map((content) => {
+                    if (content.hasOwnProperty('value')) {
+                        return content.value;
+                    }
+                    return content.content[0].value;
+                }).join('');
+                break;
+            default:
+                resultValue[key] = value;
+        }
+    });
+    return resultValue;
 };
 
 const downloadAvatar = async (url, path) => {
