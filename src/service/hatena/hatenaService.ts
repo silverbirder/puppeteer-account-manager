b59f33a1828069826e245735726e8c5d0798ea3d
shibamoto <silverbirder@gmail.com>
2020-05-19 21:44:28 +0900

    🚀 Optimize the hatena service.

--- src/service/hatena/hatenaService.ts
+++ src/service/hatena/hatenaService.ts
@@ -18,7 +18,7 @@ class HatenaService implements IService {
         await page.goto('https://www.hatena.ne.jp/login');
         switch (this.auth.name) {
             case AUTH_NAME.GOOGLE:
-                await page.waitFor('#google-signin2-button', {visible: true});
+                await page.waitFor('#google-signin2-button');
                 const [newPage] = await Promise.all([
                     browser.waitForTarget(t => t.opener() === page.target()).then(t => t.page()),
                     page.click('#google-signin2-button')
@@ -29,34 +29,28 @@ class HatenaService implements IService {
         await this.auth.dispatch();
 
         console.log(`🚀: page.goto(profile)`);
-        await page.waitFor('#profile-image-profile', {visible: true});
+        await page.waitFor('#profile-image-profile');
         await page.click('#profile-image-profile');
 
         console.log(`🚀: page.goto(edit tab)`);
-        await page.waitFor('#edit-tab > a', {visible: true});
+        await page.waitFor('#edit-tab > a');
         await page.click('#edit-tab > a');
 
         console.log(`🚀: page.click(notice dialog)`);
-        await page.waitFor('#privacy-alert-ok', {visible: true});
+        await page.waitFor('#privacy-alert-ok');
         await page.click('#privacy-alert-ok');
 
         console.log(`🚀: page.click(change image link)`);
-        await page.waitFor('td > a', {visible: true});
+        await page.waitFor('td > a');
         await page.click('td > a');
 
         const filePath: string = this.account.avatar;
         console.log(`🚀: update image ${filePath}`);
-        await page.waitFor('input[type="file"]', {visible: true});
+        await page.waitFor('input[type="file"]');
         const input: ElementHandle = await page.$('input[type="file"]');
         await input.uploadFile(filePath);
 
-        if (await page.$('#delete-profile-image') !== null) {
-            console.log(`🚀: page.click(delete image)`);
-            await page.click('#delete-profile-image');
-        }
-
         console.log(`🚀: page.click submit`);
-        await page.waitForSelector('input[type="submit"]', {visible: true});
         await page.click('input[type="submit"]');
         await page.waitForNavigation();
 
