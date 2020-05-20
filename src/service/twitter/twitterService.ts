d634b3feaa2de4060952856c0952ea717d7f0a82
shibamoto <silverbirder@gmail.com>
2020-05-21 07:27:38 +0900

    😎 Add loggging code on twitter service

--- src/service/twitter/twitterService.ts
+++ src/service/twitter/twitterService.ts
@@ -18,13 +18,21 @@ class TwitterService implements IService {
         console.log(`🚀: page.goto(twitter/login)`);
         await page.goto('https://twitter.com/login');
         await this.auth.dispatch();
+
+        console.log(`🚀: page.goto(profile page)`);
         await page.waitFor('a[aria-label="プロフィール"]');
         await page.click('a[aria-label="プロフィール"]');
+
+        console.log(`🚀: page.goto(edit profile)`);
         await page.waitFor('a[href="/settings/profile"]');
         await page.click('a[href="/settings/profile"]');
+
+        console.log(`🚀: page.goto(upload image)`);
         await page.waitFor('input[type="file"]');
         const input: ElementHandle = (await page.$$('input[type="file"]'))[1];
         await input.uploadFile(this.account.avatar);
+
+        console.log(`🚀: page.goto(save image)`);
         await page.waitForXPath('//span[text()="適用"]');
         await (await page.$x('//span[text()="適用"]'))[0].click();
         await page.waitFor('[data-testid="Profile_Save_Button"]');
